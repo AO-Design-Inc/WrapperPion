9042251d9ce782105060a3cffa0c1e3e51d77dc7
Abhishek Cherath <abhi@feather.systems>
2022-03-28 19:20:12 -0400

    added windows adaptation code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ffi = require('ffi-napi');
+const fs = require('fs');
 //const ref = require('ref-napi');
 //const Struct = require('ref-struct-di')(ref);
 
@@ -9,7 +10,22 @@ const file = join(dirname(fileURLToPath(import.meta.url)), "../dist/regodit.dll"
              .replace('app.asar', 'app.asar.unpacked'); //electron asar friendly
 */
 
-var pionjs = ffi.Library(__dirname + "/go-src/pion_handler.so", {
+var isWin = process.platform === "win32";
+let goutpath = __dirname + "/go-src/pion_handler.so";
+let libpath
+if (isWin) {
+  goutpath = __dirname + "\\go-src\\pion_handler.so";
+  libpath = __dirname + "\\go-src\\pion_handler.dll";
+  fs.rename(goutpath, libpath, (err) => {
+    if (err) throw err
+    console.log("renamed path!")
+  });
+} else {
+  goutpath = __dirname + "/go-src/pion_handler.so"
+  libpath = __dirname + "/go-src/pion_handler.so"
+}
+
+var pionjs = ffi.Library(libpath, {
   SpawnConnection: ["string", ["string"]],
   SetRemoteDescription: ["string", ["string"]]
 });
