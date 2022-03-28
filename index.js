54fdc158afee321f2c3c7a361532e639a339e023
Abhishek Cherath <abhi@feather.systems>
2022-03-28 18:15:47 -0400

    use __dirname

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const file = join(dirname(fileURLToPath(import.meta.url)), "../dist/regodit.dll"
              .replace('app.asar', 'app.asar.unpacked'); //electron asar friendly
 */
 
-var pionjs = ffi.Library("./go-src/pion_handler.so", {
+var pionjs = ffi.Library(__dirname + "/go-src/pion_handler.so", {
   SpawnConnection: ["string", ["string"]],
   SetRemoteDescription: ["string", ["string"]]
 });
 
 console.log(pionjs.SpawnConnection('[{"urls":["stun:stun.l.google.com:19302"]}]'))
 
-module.SpawnConnection = pionjs.SpawnConnection;
-module.SetRemoteDescription = pionjs.SetRemoteDescription;
+
+module.exports = pionjs
